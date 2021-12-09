c8dcf0a2ebb30504545fdb7acb6b3b2556c82c47
mizusato <mizusato@protonmail.com>
2021-12-09 13:43:46 +0900

    converter

--- logic/core.ts
+++ logic/core.ts
@@ -3,18 +3,16 @@ import {Profile,MultiValue} from './types'
 export class Converter {
     private profile: Profile
     private chars: string[]
-    private overrideChar: Map<number,string>
-    private overrideWord: Map<number,string>
-    private multiValues: [number,'char'|'word',MultiValue][]
+    private multiValues: ['char'|'word',number,MultiValue][]
+    private override: (string|null)[]
     constructor(profile: Profile, text: string) {
         this.profile = profile
         this.chars = string2chars(text)
-        this.overrideChar = new Map()
-        this.overrideWord = new Map()
         this.multiValues = this.genMultiValues()
+        this.override = []
     }
-    private genMultiValues(): [number,'char'|'word',MultiValue][] {
-        let multiValues: [number,'char'|'word',MultiValue][] = []
+    private genMultiValues(): ['char'|'word',number,MultiValue][] {
+        let multiValues: ['char'|'word',number,MultiValue][] = []
         let maxWordLength = 0
         for (let word of Object.keys(this.profile.mappingWord)) {
             let length = stringRealLength(word)
@@ -27,23 +25,45 @@ export class Converter {
             let char = this.chars[i]
             let charMultiValue = this.profile.mappingCharMulti[char]
             if (charMultiValue) {
-                multiValues.push([index, 'char', charMultiValue])
+                multiValues.push(['char', index, charMultiValue])
             }
             for (let j = 1; j < maxWordLength; j += 1) {
                 let span = this.chars.slice(i, i+j).join('')
                 let wordMultiValue = this.profile.mappingWord[span]
                 if (wordMultiValue) {
-                    multiValues.push([index, 'word', wordMultiValue])
+                    multiValues.push(['word', index, wordMultiValue])
                 }
             }
         }
         return multiValues
     }
-    output(): string {
+    MultiValues(): ['char'|'word',number,MultiValue][] {
+        return this.multiValues.slice()
+    }
+    ReadOverride(index: number): string|null {
+        return this.override[index] || null
+    }
+    WriteOverride(index: number, value: string|null) {
+        this.override[index] = value
+    }
+    Output(): string {
+        var overrideWord: Map<number,string> = new Map()
+        var overrideChar: Map<number,string> = new Map()
+        for (let i = 0; i < this.multiValues.length; i += 1) {
+            var [kind,pos] = this.multiValues[i]
+            var value = this.override[i]
+            if (value) {
+                if (kind == 'word') {
+                    overrideWord.set(pos, value)
+                } else if (kind == 'char') {
+                    overrideChar.set(pos, value)
+                }
+            }
+        }
         var parts: string[] = []
         let i = 0
         outer: while (i < this.chars.length) {
-            for (let override of [this.overrideWord, this.overrideChar]) {
+            for (let override of [overrideWord, overrideChar]) {
                 if (override.has(i)) {
                     let converted = override.get(i)!
                     let length = stringRealLength(converted)
